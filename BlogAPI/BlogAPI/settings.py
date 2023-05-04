ca9c03775a95e8e5d0a940be25cf648e3a4ef23a
Arsha-Meenu <arsha.mailme@gmail.com>
2023-05-04 12:22:53 +0530

    cors change commit

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -146,6 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000/",
+# ]
+
+
+
+CORS_ALLOW_ALL_ORIGINS =True
